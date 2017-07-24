ab8d7efb88db9717a529e9886247e65bc21db133
Ashok Karale <Ashok.Karale@outlook.com>
2017-07-24 20:59:44 +0530

    Method overloading implemented.

--- AcmeApp/Tests/Acme.BizTests/VendorTests.cs
+++ AcmeApp/Tests/Acme.BizTests/VendorTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Acme.Common;
 
 namespace Acme.Biz.Tests
 {
@@ -54,6 +55,49 @@ namespace Acme.Biz.Tests
 
             // Assert
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void PlaceOrderTest()
+        {
+            //Arrenge
+            var vendor = new Vendor();
+            var product = new Product(1, "Saw", "");
+            var expected = new OperationResult(true, "Order from Acme, Inc\r\nProduct: Tools-1\r\nQuantity: 12");
+
+            //Act
+            var actual = vendor.PlaceOrder(product, 12);
+
+            //Assert
+            Assert.AreEqual(expected.Success, actual.Success);
+            Assert.AreEqual(expected.Message, actual.Message);
+        }
+
+        [TestMethod()]
+        public void PlaceOrder_3Parameters()
+        {
+            //Arrenge
+            var vendor = new Vendor();
+            var product = new Product(1, "Saw", "");
+            var expected = new OperationResult(true, "Order from Acme, Inc\r\nProduct: Tools-1\r\nQuantity: 12"+"\r\nDeliver By: 10/25/17"); ;
+
+            //Act
+            var actual = vendor.PlaceOrder(product, 12, new DateTimeOffset(2017, 10, 25, 0, 0, 0, new TimeSpan(-7, 0, 0)));
+
+            //Assert
+            Assert.AreEqual(expected.Success, actual.Success);
+            Assert.AreEqual(expected.Message, actual.Message);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PlaceOrder_NullProduct_Exception()
+        {
+            //Arrenge
+            var vendor = new Vendor();
+
+            //Act
+            var actual = vendor.PlaceOrder(null, 12);
         }
     }
 }
